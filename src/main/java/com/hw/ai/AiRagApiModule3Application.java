2b7a4e9ca92d93ec4f3442b3d4fd783f1b600810
Pimenova, Marina <mnfpma@gmail.com>
2026-02-22 19:29:53 +0200

    Init

---
+++ src/main/java/com/hw/ai/AiRagApiModule3Application.java
@@ -0,0 +1,13 @@
+package com.hw.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRagApiModule3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRagApiModule3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hw/ai/AiRagApiModule3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hw.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRagApiModule3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
